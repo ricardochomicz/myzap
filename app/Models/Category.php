5c23cc52ab0624d27806e50156f29f6fa711d677
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2022-09-01 17:55:47 -0300

    Relation Category x Product

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,4 +10,9 @@ class Category extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'slug', 'active'];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -17,4 +17,9 @@ class Product extends Model
         'stock',
         'active'
     ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
