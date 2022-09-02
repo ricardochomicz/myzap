bd8e20a6739b11cb78cf0ce9d03b5fb878260fe2
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2022-09-02 10:52:54 -0300

    Input stock products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,17 +19,29 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         \App\Models\Category::factory(10)->create();
-        
+
         //retornar todas as categorias
         $categories = Category::all();
         \App\Models\Product::factory(20)
-        ->create()
-        ->each(function(Product $product) use ($categories){
-            //pega categoria aleatoria
-            $categoryId = $categories->random()->id;
-            //inclui a categoria no relacionamento
-            $product->categories()->attach($categoryId);
-        });
+            ->create()
+            ->each(function (Product $product) use ($categories) {
+                //pega categoria aleatoria
+                $categoryId = $categories->random()->id;
+                //inclui a categoria no relacionamento
+                $product->categories()->attach($categoryId);
+            });
+
+        $products = \App\Models\Product::all();
+        \App\Models\ProductInput::factory(150)
+            ->make()
+            ->each(function ($input) use ($products) {
+                $product = $products->random();
+                $input->product_id = $product->id;
+                $input->save();
+                $product->stock += $input->amount;
+                $product->save();
+
+            });
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
