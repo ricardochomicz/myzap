5c5fc49a108946371a813d76f0beb2a116e4e674
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2022-09-08 11:04:14 -0300

    Authentication Api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\{
+    AuthController,
     CategoryController,
     ProductCategoryController,
     ProductController,
@@ -14,14 +15,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['as' => 'api.'], function () {
-    Route::resource('categories', CategoryController::class, ['except' => ['edit', 'create']]);
-    Route::patch('products/{product}/restore', [ProductController::class, 'restore']);
-    Route::resource('products', ProductController::class, ['except' => ['edit', 'create']]);
-    Route::resource('products.categories', ProductCategoryController::class, ['only' => ['index', 'store', 'destroy']]);
-    Route::resource('products.photos', ProductPhotoController::class, ['except' => ['create', 'edit']]);
-    Route::resource('inputs', ProductInputController::class, ['only' => ['index', 'store', 'show']]);
-    Route::resource('outputs', ProductOutputController::class, ['only' => ['index', 'store', 'show']]);
-    Route::resource('users', UserController::class, ['except' => ['edit', 'create']]);
+    Route::name('register')->post('register', [AuthController::class, 'register']);
+    Route::name('login')->post('login', [AuthController::class, 'login']);
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+        Route::resource('categories', CategoryController::class, ['except' => ['edit', 'create']]);
+        Route::patch('products/{product}/restore', [ProductController::class, 'restore']);
+        Route::resource('products', ProductController::class, ['except' => ['edit', 'create']]);
+        Route::resource('products.categories', ProductCategoryController::class, ['only' => ['index', 'store', 'destroy']]);
+        Route::resource('products.photos', ProductPhotoController::class, ['except' => ['create', 'edit']]);
+        Route::resource('inputs', ProductInputController::class, ['only' => ['index', 'store', 'show']]);
+        Route::resource('outputs', ProductOutputController::class, ['only' => ['index', 'store', 'show']]);
+        Route::resource('users', UserController::class, ['except' => ['edit', 'create']]);
+    });
 });
 
 
